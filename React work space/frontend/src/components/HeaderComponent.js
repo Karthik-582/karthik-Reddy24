dcf69de2604e19989c1bffb367de185a23ba9c27
Karthik-582 <mopatikarthik@gmail.com>
2025-04-18 15:04:16 +0530

    Initial commit

---
+++ React work space/frontend/src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+function HeaderComponent()
+{
+    return(
+         <header>
+             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                 <a href="" className="navbar-brand">Tech Mahindra</a>     
+                </div> 
+             </nav>       
+         </header>   
+    )
+}
+export default HeaderComponent;
